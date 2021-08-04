2c13fc01589101507896dfefcd879e12e4ac9328
l3lackMegas <jara7897@gmail.com>
2021-08-04 20:48:10 +0700

    feat: add repo link

--- components/Page.tsx
+++ components/Page.tsx
@@ -88,10 +88,18 @@ class Page extends Component<IReciept> {
                 </Link>
                 <span>. </span>
                 <p className="blockMobile">
+                    <span>View source code on </span>
+                    <Link href="https://github.com/l3lackMegas/l3lackmegas.github.io">
+                        <a target="_blank" rel="noreferrer">GitHub</a>
+                    </Link>
+                    <span>. </span>
+                </p>
+                <p className="blockMobile">
                     <span>Background Image by </span>
                     <Link href="http://www.freepik.com">
                         <a target="_blank" rel="noreferrer">fanjianhua / Freepik</a>
                     </Link>
+                    <span>.</span>
                 </p>
             </div>
         </>
