49be17147fcac7c6d3ac31d772af2af462c36da3
l3lackMegas <jara7897@gmail.com>
2021-09-12 03:26:50 +0700

    feat: implement animation for preloader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,10 +40,30 @@ class MyApp extends App<AppProps> {
                     <title>l3lackMegas</title>
                 </Head>
                 <AnimateSharedLayout type='crossfade'>
-                    <AnimatePresence exitBeforeEnter onExitComplete={()=>{ window.scroll(0, 0)}}>
+                    <AnimatePresence exitBeforeEnter>
                         {!isLoaded &&
-                            <PreLoader>&lt;/&gt; Just a sec.</PreLoader>
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{
+                                    opacity: 0,
+                                    scale: .5,
+                                    transition:{ duration: .75 }
+                                }}
+                                style={{
+                                    position: 'fixed',
+                                    top: 0,
+                                    left: 0,
+                                    width: '100%',
+                                    height: '100%'
+                                }}
+                                transition={{ duration: .5 }}
+                            >
+                                <PreLoader>&lt;/&gt; Just a sec.</PreLoader>
+                            </motion.div>
                         }
+                    </AnimatePresence>
+                    <AnimatePresence exitBeforeEnter onExitComplete={()=>{ window.scroll(0, 0)}}>
                         {isLoaded &&
                             <SmoothScroll>
                                 <Component {...pageProps} key={router.route}  />
