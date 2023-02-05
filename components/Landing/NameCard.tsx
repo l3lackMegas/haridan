97c8701dbc4188a823e9d4cee13a46431147efe2
l3lackMegas ༼ つ ◕_◕ ༽つ <contact@jaruwat.dev>
2023-02-05 19:05:33 +0700

    Revert "feat: add I leaned css"
    This reverts commit 3611ac8d18532cffea39c5985f27e1e3d43374f6.

--- components/Landing/NameCard.tsx
+++ components/Landing/NameCard.tsx
@@ -16,7 +16,6 @@ import styles from './styles.module.css'
 interface IReciept {
     isReady: Boolean
     nextFn?: Function
-    onHover: Function
 }
 
 class NameCard extends Component<IReciept> {
@@ -25,74 +24,67 @@ class NameCard extends Component<IReciept> {
 
         const { isReady } = this.props
         
-        return <>
-            <motion.div className={styles.nameCard}
-                initial={{ opacity: 0, scale: .5 }}
-                animate={ isReady ? { opacity: 1, scale: 1 } : {}}
-                transition={{ duration: .65 }}
+        return <motion.div className={styles.nameCard}
+            initial={{ opacity: 0, scale: .5 }}
+            animate={ isReady ? { opacity: 1, scale: 1 } : {}}
+            transition={{ duration: .65 }}
+        >
+            <motion.div
+                initial={{ opacity: 0, y: 200 }}
+                animate={ isReady ? { opacity: 1, y: 0 } : {}}
+                transition={{ duration: .75 }}
             >
-                <motion.div
-                    initial={{ opacity: 0, y: 200 }}
-                    animate={ isReady ? { opacity: 1, y: 0 } : {}}
-                    transition={{ duration: .75 }}
-                >
-                    <div className={styles.circlePic}>
-                        <motion.div
-                            className={styles.img}
-                            style={{ backgroundImage: 'url(/img/profile-portrait.jpg)' }}
-                            onHoverStart={() => this.props.onHover(true)}
-                            onHoverEnd={() => this.props.onHover(false)}
-                            onTouchStart={() => this.props.onHover(true)}
-                            onTouchEnd={() => this.props.onHover(false)}
-                        ></motion.div>
-                    </div>
-                    <div className={styles.identity}>
-                        <p className={styles.name}>JARUWAT POHONG</p>
-                        <p className={styles.subName}>Software Developer</p>
-                    </div>
-                    <div className={styles.statusQuote}>
-                        <p><span>“</span>Cluttered code by me.<span>”</span></p>
-                    </div>
-                </motion.div>
-                <motion.div className={styles.buttonPanel}
-                    initial={{ opacity: 0, y: 200 }}
-                    animate={ isReady ? { opacity: 1, y: 0 } : {}}
-                    transition={{ delay: .1, duration: .75 }}
-                >
-                    <div style={{ margin: '20px 0'}}>
-                        <Link href="mailto:contact@jaruwat.dev">
-                            <a>contact@jaruwat.dev</a>
-                        </Link>
-                    </div>
-                    <Link href="https://m.me/Jaruwat.P">
-                        <a target="_blank" rel="noreferrer">
-                            <button className={`${styles.btn} ${styles.facebook}`}>
-                                <FontAwesomeIcon icon={faFacebookMessenger} />
-                                <span>Messenger</span>
-                            </button>
-                        </a>
-                    </Link>
-                    <Link href="https://github.com/l3lackMegas">
-                        <a target="_blank" rel="noreferrer">
-                            <button className={`${styles.btn} ${styles.github}`}>
-                                <FontAwesomeIcon icon={faGithub} />
-                                <span>GitHub</span>
-                            </button>
-                        </a>
-                    </Link>
-                    <Link href="/docs/Jaruwat Pohong - Resume.pdf">
-                        <a target="_blank" rel="noreferrer">
-                            <button
-                                className={styles.btn}
-                            >
-                                <FontAwesomeIcon icon={faDownload} />
-                                <span>Résumé</span>
-                            </button>
-                        </a>
-                    </Link>
-                </motion.div>
+                <div className={styles.circlePic}>
+                    <div className={styles.img}
+                        style={{ backgroundImage: 'url(/img/profile-portrait.jpg)' }}
+                    ></div>
+                </div>
+                <div className={styles.identity}>
+                    <p className={styles.name}>JARUWAT POHONG</p>
+                    <p className={styles.subName}>Software Developer</p>
+                </div>
+                <div className={styles.statusQuote}>
+                    <p><span>“</span>Cluttered code by me.<span>”</span></p>
+                </div>
             </motion.div>
-        </>
+            <motion.div className={styles.buttonPanel}
+                initial={{ opacity: 0, y: 200 }}
+                animate={ isReady ? { opacity: 1, y: 0 } : {}}
+                transition={{ delay: .1, duration: .75 }}
+            >
+                <div style={{ margin: '20px 0'}}>
+                    <Link href="mailto:contact@jaruwat.dev">
+                        <a>contact@jaruwat.dev</a>
+                    </Link>
+                </div>
+                <Link href="https://m.me/Jaruwat.P">
+                    <a target="_blank" rel="noreferrer">
+                        <button className={`${styles.btn} ${styles.facebook}`}>
+                            <FontAwesomeIcon icon={faFacebookMessenger} />
+                            <span>Messenger</span>
+                        </button>
+                    </a>
+                </Link>
+                <Link href="https://github.com/l3lackMegas">
+                    <a target="_blank" rel="noreferrer">
+                        <button className={`${styles.btn} ${styles.github}`}>
+                            <FontAwesomeIcon icon={faGithub} />
+                            <span>GitHub</span>
+                        </button>
+                    </a>
+                </Link>
+                <Link href="/docs/Jaruwat Pohong - Resume.pdf">
+                    <a target="_blank" rel="noreferrer">
+                        <button
+                            className={styles.btn}
+                        >
+                            <FontAwesomeIcon icon={faDownload} />
+                            <span>Résumé</span>
+                        </button>
+                    </a>
+                </Link>
+            </motion.div>
+        </motion.div>
     }
 
 }
--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -5,7 +5,7 @@ import React, { Component } from "react";
 import Link from 'next/link'
 
 /* External Module */
-import { AnimatePresence, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
@@ -40,10 +40,6 @@ class Landing extends Component<IReciept> {
 
     constructor(props: IReciept) {
         super(props)
-    }
-
-    state = {
-        onHover: false
     }
 
     render() {
@@ -116,20 +112,10 @@ class Landing extends Component<IReciept> {
                 </motion.div>
                 </div>
             </div>
-            { this.state.onHover && <AnimatePresence exitBeforeEnter={true}>
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 1 }}
-                    className={styles.iLeanedCss}
-                ></motion.div>
-            </AnimatePresence>
-            }
             <div className={`centerContain ${styles.cardMobile}`} style={{
                 zIndex: 1000
             }}>
-                <NameCard isReady={isReady} onHover={(hover: Boolean) => this.setState({onHover: hover})}/>
+                <NameCard isReady={isReady} />
             </div>
         </>
     }
